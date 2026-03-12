2bfc0a000c54f4a756fc2a8bc74f3309bf6b4bbc
EfimovSergeiV <efimovsergeiv@gmail.com>
2026-03-12 10:04:02 +0300

    add:

--- server/server.py
+++ server/server.py
@@ -1,38 +1,51 @@
-import os, asyncio, secrets
-from fastapi import FastAPI
+import os, asyncio, secrets, requests
+from fastapi import FastAPI, Request
 from datetime import datetime
 
 from tg_alert import notify
 
 app = FastAPI()
 
+
 def getenv(name, default=None, random_len=None):
+    """ Возвращает переменную окружения """
     value = os.getenv(name)
 
     if value:
         return value
-
     if random_len:
         return secrets.token_hex(random_len)
 
     return default
 
 
-# переменные окружения
+
 TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN", random_len=16)
-TELEGRAM_CHAT_ID = getenv("TELEGRAM_CHAT_ID", default="0")
 PING_TIMEOUT = int(getenv("PING_TIMEOUT", default="180"))
 
-print(f"TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
-print(f"TELEGRAM_CHAT_ID: {TELEGRAM_CHAT_ID}")
-print(f"PING_TIMEOUT: {PING_TIMEOUT}")
+
+def get_chat_ids():
+    response = requests.get(f"https://api.telegram.org/bot{ TELEGRAM_TOKEN }/getUpdates").json()
+    messages = response.get("result")
+    
+    if messages:
+
+        for msg in messages:
+            chat = msg.get("message").get("chat")
+            id, username = chat.get("id"), chat.get("username")
+
+            if id not in chat_ids:
+                chat_ids.append(id)
+
 
 last_ping = None
 power_off_notified = False
-
+chat_ids = []
 
 async def monitor():
-    global last_ping, power_off_notified
+    global last_ping, power_off_notified, chat_ids
+
+    get_chat_ids()
 
     while True:
         await asyncio.sleep(60)
@@ -44,12 +57,12 @@ async def monitor():
 
         if diff > PING_TIMEOUT and not power_off_notified:
             print("Power OFF")
-            notify(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, "⚠️ Power OFF detected!")
+            notify(TELEGRAM_TOKEN, chat_ids, "⚠️ Power OFF detected!")
             power_off_notified = True
 
         if diff <= PING_TIMEOUT and power_off_notified:
             print("Power ON")
-            notify(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, "✅ Power ON detected!")
+            notify(TELEGRAM_TOKEN, chat_ids, "✅ Power ON detected!")
             power_off_notified = False
 
 
@@ -62,5 +75,5 @@ async def startup():
 async def ping():
     global last_ping
     last_ping = datetime.utcnow()
-    notify(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, "📶 Ping received!")
+    # notify(TELEGRAM_TOKEN, chat_ids, "📶 Ping received!")
     return {"status": "ok"}
--- server/tg_alert.py
+++ server/tg_alert.py
@@ -1,10 +1,13 @@
 import requests
 
 
-def notify(BOT_TOKEN, CHAT_ID, text):
-    print("Notify: ", BOT_TOKEN, CHAT_ID, text)
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, json={
-        "chat_id": CHAT_ID,
-        "text": text
-    })
+
+def notify(BOT_TOKEN, CHAT_IDS, text):
+    print("Notify: ", BOT_TOKEN, CHAT_IDS, text)
+    url = f"https://api.telegram.org/bot{ BOT_TOKEN }/sendMessage"
+    
+    for chat_id in CHAT_IDS:
+        requests.post(url, json={
+            "chat_id": chat_id,
+            "text": text
+        })
